8899b253c0d59dee78eee04f768778242cd6afdd
Luuk Veenis <luuk@stembolt.com>
2017-07-31 13:53:02 -0700

    Print when routes are initialized

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+puts "\n" * 20
+puts "Loading #{__FILE__}. Backtrace:"
+puts caller.join("\n")
+puts "\n" * 20
+
 Spree::Core::Engine.routes.draw do
   post "/paybright/callback", to: "paybright#callback", as: :paybright_callback
   get "/paybright/cancel/:payment_id", to: "paybright#cancel", as: :paybright_cancel
